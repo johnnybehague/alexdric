bc79e9aabd78efe003d8903ebf00b64934727ba2
Johnny Behague <johnny.behague@live.fr>
2025-04-16 15:02:45 +0200

    test: add tests for Application/Common classes

---
+++ Alexdric/Alexdric.Tests/Application/Common/BaseError.Tests.cs
@@ -0,0 +1,42 @@
+﻿using Alexdric.Application.Common;
+
+namespace Alexdric.Tests.Application.Common;
+
+[TestClass]
+public class BaseErrorTests
+{
+    #region Constructor Tests
+
+    [TestMethod]
+    public void Constructor_ShouldReturnsNewBaseError_WhenNoParameters()
+    {
+        // Act
+        var error = new BaseError();
+
+        // Assert
+        Assert.IsNotNull(error);
+        Assert.IsNull(error.PropertyMessage);
+        Assert.IsNull(error.ErrorMessage);
+    }
+
+    [TestMethod]
+    public void Constructor_ShouldSetParameters_WhenParametersAreGiven()
+    {
+        // Arrange
+        var expectedProperty = "FieldName";
+        var expectedError = "Field is required";
+
+        // Act
+        var error = new BaseError
+        {
+            PropertyMessage = expectedProperty,
+            ErrorMessage = expectedError
+        };
+
+        // Assert
+        Assert.AreEqual(expectedProperty, error.PropertyMessage);
+        Assert.AreEqual(expectedError, error.ErrorMessage);
+    }
+
+    #endregion
+}
---
+++ Alexdric/Alexdric.Tests/Application/Common/BaseResponse.Tests.cs
@@ -0,0 +1,68 @@
+﻿using Alexdric.Application.Common;
+
+namespace Alexdric.Tests.Application.Common;
+
+[TestClass]
+public class BaseResponseTests
+{
+    [TestMethod]
+    public void Constructor_ShouldReturnsResponseWithDefaultValues_WhenNoParameters()
+    {
+        // Act
+        var response = new BaseResponse<string>();
+
+        // Assert
+        Assert.IsNotNull(response);
+        Assert.IsFalse(response.Succcess);
+        Assert.IsNull(response.Data);
+        Assert.IsNull(response.Message);
+        Assert.IsNull(response.Errors);
+    }
+
+    [TestMethod]
+    public void Constructor_ShouldReturnsResponseWithSetValues_WhenParametersAreNotEmpty()
+    {
+        // Arrange
+        var data = "Some test data";
+        var message = "All good!";
+        var errors = new List<BaseError>
+            {
+                new BaseError { PropertyMessage = "Field1", ErrorMessage = "Required" }
+            };
+
+        // Act
+        var response = new BaseResponse<string>
+        {
+            Succcess = true,
+            Data = data,
+            Message = message,
+            Errors = errors
+        };
+
+        // Assert
+        Assert.IsTrue(response.Succcess);
+        Assert.AreEqual(data, response.Data);
+        Assert.AreEqual(message, response.Message);
+        Assert.AreEqual(1, response.Errors.Count());
+        Assert.AreEqual("Field1", response.Errors.First().PropertyMessage);
+    }
+
+    [TestMethod]
+    public void Constructor_ShouldReturnsResponseWitNullValues_WhenParametersAreNull()
+    {
+        // Act
+        var response = new BaseResponse<object>
+        {
+            Succcess = false,
+            Data = null,
+            Message = null,
+            Errors = null
+        };
+
+        // Assert
+        Assert.IsFalse(response.Succcess);
+        Assert.IsNull(response.Data);
+        Assert.IsNull(response.Message);
+        Assert.IsNull(response.Errors);
+    }
+}
